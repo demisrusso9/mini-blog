8b8ade699ebf8de7645853709c13e81b5aa6e918
Demis Russo <demisjunior@hotmail.com>
2025-03-21 20:22:57 -0300

    feat: enhance about page with content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,20 @@
+import { NavLink } from 'react-router'
+
 export function About() {
-	return <div className="bg-gray-500">About Page</div>
+	return (
+		<div className="flex w-full flex-col items-center justify-center bg-gray-300">
+			<h2 className="mb-4 text-2xl font-bold">Sobre o Mini BLog</h2>
+
+			<p className="mb-4 text-base font-thin">
+				Esse projeto consiste em um blog feito com React no front-end e Firebase
+				no back-end
+			</p>
+
+			<NavLink to="/">
+				<button className="cursor-pointer rounded-sm border p-2 hover:bg-black hover:text-gray-100">
+					Criar post
+				</button>
+			</NavLink>
+		</div>
+	)
 }
