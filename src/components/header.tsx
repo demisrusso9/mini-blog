7f809c2bdc48e3260809265efe82208e6cfac0d0
Demis Russo <demisjunior@hotmail.com>
2025-03-23 16:37:43 -0300

    feat: refactor header component to use dynamic navigation links and add active link css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,16 @@ import { NavLink } from 'react-router'
 
 export function Header() {
 	const navLinkCssClasses =
-		'text-black p-2 rounded-sm hover:bg-black hover:p-2 hover:text-gray-100'
+		'rounded-sm p-2 hover:bg-black hover:p-2 hover:text-gray-100'
+
+	const navLinkActive = `rounded-sm bg-black p-2 hover:p-2 text-gray-100`
+
+	const navLinks = [
+		{ route: '/', name: 'Home' },
+		{ route: '/login', name: 'Entrar' },
+		{ route: '/register', name: 'Cadastrar' },
+		{ route: '/about', name: 'Sobre' }
+	]
 
 	return (
 		<header className="flex h-20 items-center justify-between bg-gray-200 p-4">
@@ -10,22 +19,16 @@ export function Header() {
 				Mini <strong>Blog</strong>
 			</h1>
 
-			<nav className="flex items-center gap-4">
-				<NavLink to="/">
-					<span className={navLinkCssClasses}>Home</span>
-				</NavLink>
-
-				<NavLink to="/login">
-					<span className={navLinkCssClasses}>Entrar</span>
-				</NavLink>
-
-				<NavLink to="/register">
-					<span className={navLinkCssClasses}>Cadastrar</span>
-				</NavLink>
-
-				<NavLink to="/about">
-					<span className={navLinkCssClasses}>Sobre</span>
-				</NavLink>
+			<nav className="flex items-center gap-5">
+				{navLinks.map((navLink) => (
+					<NavLink
+						key={navLink.route}
+						to={navLink.route}
+						className={({ isActive }) => (isActive ? navLinkActive : '')}
+					>
+						<span className={navLinkCssClasses}>{navLink.name}</span>
+					</NavLink>
+				))}
 			</nav>
 		</header>
 	)
