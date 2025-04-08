c226dbe049236569e7b2e658743d6353e1d9b5ec
Demis Russo <demisjunior@hotmail.com>
2025-04-07 23:07:28 -0300

    feat: add user authentication check on About page navigation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,19 @@
-import { NavLink } from 'react-router'
+import { getUser } from '@/api/get-user'
+import { NavLink, useNavigate } from 'react-router'
 
 export function About() {
+	const navigate = useNavigate()
+
+	async function handleNavigation() {
+		const currentUser = await getUser()
+
+		if (currentUser) {
+			navigate('/create-post')
+		} else {
+			navigate('/login')
+		}
+	}
+
 	return (
 		<div className="flex w-full flex-col items-center justify-center bg-gray-300">
 			<h2 className="mb-4 text-2xl font-bold">Sobre o Mini BLog</h2>
@@ -10,7 +23,7 @@ export function About() {
 				no back-end
 			</p>
 
-			<NavLink to="/">
+			<NavLink to="/" onClick={handleNavigation}>
 				<button className="cursor-pointer rounded-sm border p-2 hover:bg-black hover:text-gray-100">
 					Criar post
 				</button>
